d11c2652c61a9c7c4c6d7bfe52ab85c785a40b2c
jesus462 <jesusponce.462@gmail.com>
2022-12-18 22:56:20 -0400

    changed personal api_key to demo_key

--- src/services/nasa.ts
+++ src/services/nasa.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { INasaPhotos } from "../types";
 
-const nasa_api_key = process.env.REACT_APP_API_KEY;
-const baseUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2012-10-10&api_key=${nasa_api_key}`;
+const baseUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2012-10-10&api_key=DEMO_KEY`;
 
 const getPhotos = () => {
   const request = axios.get(baseUrl);
